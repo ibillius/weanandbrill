623a384f84f2db3f946541d45ccddda0f5b18794
ibillius <wss.simpson@gmail.com>
2013-03-08 16:14:55 -0700

    Basic reponsive layout working.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+(function($){
+  $(document).ready(function(){
+    var height = $('.wab_content').height();
+
+    console.log('Height: '+height);
+    $('#content-border').height(height);
+    //$('#content-border').width('940px');
+
+    $(window).resize(function(){
+        console.log($('.wab_content').height());
+        $('#content-border').height($('.wab_content').outerHeight());
+    });
+  });
+})(jQuery);
 
