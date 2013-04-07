69c0cd4fc7a1d77b29ca2f242a92126df9a571ea
ibillius <wss.simpson@gmail.com>
2013-04-07 02:26:09 -0700

    Changed shade of blue, moved header text up.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 (function($){
   $(window).load(function(){
     $('#content-border').height($('#content_row').outerHeight(true));
+    $('.wab_header').height($('#header_image').outerHeight(true));
+    console.log($('#header_image').outerHeight(true));
     $(window).resize(function(){
-        console.log("Outer Height: "+$('#content_row').outerHeight(true));
         $('#content-border').height($('#content_row').outerHeight(true));
+        $('.wab_header').height($('#header_image').outerHeight(true));
     });
   });
 })(jQuery);
