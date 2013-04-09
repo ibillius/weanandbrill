3deb7dca04816c85bc47ab56d30916710fbd1bbd
ibillius <wss.simpson@gmail.com>
2013-04-08 20:57:13 -0700

    Possibly fixed header issue with narrow viewports, changed secondary font.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 (function($){
   $(window).load(function(){
     $('#content-border').height($('#content_row').outerHeight(true));
-    $('.wab_header').height($('#header_image').outerHeight(true));
+    $('#header_image').height($('.header_row').outerHeight(true));
     console.log($('#header_image').outerHeight(true));
     $(window).resize(function(){
         $('#content-border').height($('#content_row').outerHeight(true));
-        $('.wab_header').height($('#header_image').outerHeight(true));
+        $('#header_image').height($('.header_row').outerHeight(true));
     });
   });
 })(jQuery);
