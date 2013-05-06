83a81587f06be432e63065e464e6b68a330fab5d
ibillius <wss.simpson@gmail.com>
2013-05-05 19:22:15 -0700

    Fixed a typo in main.js.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 (function($){
   $(window).load(function(){
     $('#content-border').height($('#content_row').outerHeight(true));
-    $('#header_image').height($('.header_row').outerHeight(true));
+    $('#header_image').height($('#header_row').outerHeight(true));
     console.log($('#header_image').outerHeight(true));
     $(window).resize(function(){
         $('#content-border').height($('#content_row').outerHeight(true));
